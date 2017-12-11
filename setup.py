d533052a337779c6f30b98485fb468fe7032f404
Fabio Manganiello <blacklight86@gmail.com>
2017-12-11 20:31:42 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ create_etc_dir()
 
 setup(
     name = "platypush",
-    version = "0.2.2.dev2",
+    version = "0.3",
     author = "Fabio Manganiello",
     author_email = "info@fabiomanganiello.com",
     description = ("Platypush service"),
