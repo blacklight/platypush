9220b2347179bf48742b791ec48a0a3f02dfc5a3
Fabio Manganiello <blacklight86@gmail.com>
2018-01-02 19:34:41 +0100

    Removed two unneeded rows

--- platypush/pusher/__main__.py
+++ platypush/pusher/__main__.py
@@ -4,12 +4,9 @@ from . import Pusher
 
 def main(args=sys.argv[1:]):
     opts = Pusher.parse_build_args(args)
-
     pusher = Pusher(config_file=opts.config, backend=opts.backend)
 
     if opts.type == 'event':
-        delattr(opts, 'type')
-        print(opts.args)
         pusher.send_event(target=opts.target, type=opts.event, **opts.args)
     else:
         pusher.send_request(target=opts.target, action=opts.action, timeout=opts.timeout, **opts.args)
