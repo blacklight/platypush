a16d92ae8ec95fa958e6507951999fea985d54fe
Fabio Manganiello <blacklight86@gmail.com>
2018-01-15 22:46:41 +0100

    Version bump

--- platypush/__init__.py
+++ platypush/__init__.py
@@ -16,7 +16,7 @@ from .message.response import Response
 
 
 __author__ = 'Fabio Manganiello <blacklight86@gmail.com>'
-__version__ = '0.6.9'
+__version__ = '0.7.0'
 
 #-----------#
 
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ create_etc_dir()
 
 setup(
     name = "platypush",
-    version = "0.6.9",
+    version = "0.7.0",
     author = "Fabio Manganiello",
     author_email = "info@fabiomanganiello.com",
     description = ("Platypush service"),
