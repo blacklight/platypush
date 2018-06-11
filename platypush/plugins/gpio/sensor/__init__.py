bdf862e6d2c68404bee000d563ef620945cbd47f
Fabio Manganiello <blacklight86@gmail.com>
2018-06-12 00:46:51 +0200

    get_data alias for get_measuremnt

--- platypush/plugins/gpio/sensor/__init__.py
+++ platypush/plugins/gpio/sensor/__init__.py
@@ -8,6 +8,8 @@ class GpioSensorPlugin(Plugin):
     def get_measurement(self, *args, **kwargs):
         raise NotImplementedError('get_measurement should be implemented in a derived class')
 
+    def get_data(self, *args, **kwargs):
+        return self.get_measurement(*args, **kwargs)
 
 # vim:sw=4:ts=4:et:
 
