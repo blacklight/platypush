5b5e861f94e3c3172e1cd4c49c599c552f3285b1
Fabio Manganiello <blacklight86@gmail.com>
2018-09-06 03:02:16 +0200

    Implemented distance sensors backend

---
+++ platypush/backend/sensor/distance/__init__.py
@@ -0,0 +1,23 @@
+from platypush.backend.sensor import SensorBackend
+from platypush.context import get_plugin
+
+
+class SensorDistanceBackend(SensorBackend):
+    """
+    Backend to poll a distance sensor.
+
+    Requires:
+
+        * ``RPi.GPIO`` (``pip install RPi.GPIO``)
+        * The :mod:`platypush.plugins.gpio.sensor.distance` plugin configured
+    """
+
+    def get_measurement(self):
+        """ get_measurement implementation """
+        plugin = get_plugin('gpio.sensor.distance')
+        return plugin.get_data().output
+
+
+# vim:sw=4:ts=4:et:
+
+
