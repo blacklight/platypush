475f6d3f6f4dac2ba7d9fb253d0b4878357a6936
Fabio Manganiello <blacklight86@gmail.com>
2018-10-15 22:12:30 +0200

    Version bump 0.9 -> 0.9.1

--- platypush/__init__.py
+++ platypush/__init__.py
@@ -24,7 +24,7 @@ from .message.response import Response
 
 
 __author__ = 'Fabio Manganiello <blacklight86@gmail.com>'
-__version__ = '0.9'
+__version__ = '0.9.1'
 
 LOGGER = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ backend = pkg_files('platypush/backend')
 
 setup(
     name = "platypush",
-    version = "0.9",
+    version = "0.9.1",
     author = "Fabio Manganiello",
     author_email = "info@fabiomanganiello.com",
     description = ("Platypush service"),
