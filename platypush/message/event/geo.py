710694e8d7f96356ca7eea6ac7402a402c254abd
Fabio Manganiello <blacklight86@gmail.com>
2019-03-18 00:17:11 +0100

    Added altitude to LatLongUpdateEvent

--- platypush/message/event/geo.py
+++ platypush/message/event/geo.py
@@ -6,17 +6,19 @@ class LatLongUpdateEvent(Event):
     Event triggered upon GPS location update
     """
 
-    def __init__(self, latitude, longitude, *args, **kwargs):
+    def __init__(self, latitude, longitude, altitude=None, *args, **kwargs):
         """
         :param latitude: GPS latitude
         :type latitude: float
 
         :param longitude: GPS longitude
         :type longitude: float
+
+        :param altitude: GPS altitude
+        :type altitude: float
         """
 
-        super().__init__(latitude=latitude, longitude=longitude, *args, **kwargs)
+        super().__init__(*args, latitude=latitude, longitude=longitude, altitude=altitude, **kwargs)
 
 
 # vim:sw=4:ts=4:et:
-
