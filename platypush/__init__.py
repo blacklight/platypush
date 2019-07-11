6848a2a0c1dfb4002085cf077fe325e5931fe457
Fabio Manganiello <blacklight86@gmail.com>
2019-07-12 00:59:03 +0200

    Version bump 0.10.2 -> 0.10.3

--- platypush/__init__.py
+++ platypush/__init__.py
@@ -28,7 +28,7 @@ from .utils import set_thread_name
 
 
 __author__ = 'Fabio Manganiello <blacklight86@gmail.com>'
-__version__ = '0.10.2'
+__version__ = '0.10.3'
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
