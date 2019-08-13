d4800b5c5554619fe21cdba597e9aa3eceecd95a
Fabio Manganiello <blacklight86@gmail.com>
2019-08-13 22:25:53 +0200

    Ignore/mock picamera import in readthedocs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -222,6 +222,7 @@ autodoc_mock_imports = ['googlesamples.assistant.grpc.audio_helpers',
                         'grpc',
                         'envirophat',
                         'gps',
+                        'picamera',
                         ]
 
 sys.path.insert(0, os.path.abspath('../..'))
