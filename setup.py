f6a199f77bef89c5e2dc9f45531ab7b2fb07d083
Fabio Manganiello <blacklight86@gmail.com>
2019-12-04 12:26:26 +0000

    bump

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ backend = pkg_files('platypush/backend')
 
 setup(
     name = "platypush",
-    version = "0.10.9",
+    version = "0.11.0",
     author = "Fabio Manganiello",
     author_email = "info@fabiomanganiello.com",
     description = ("Platypush service"),
