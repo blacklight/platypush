3bb4c4434c0e17b3abac303e3c75f72ad81bd821
Fabio Manganiello <blacklight86@gmail.com>
2020-04-30 00:44:34 +0200

    Added Covid-19 country stats plugin and backend/monitor

---
+++ platypush/message/event/covid19.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Optional
+
+from platypush.message.event import Event
+
+
+class Covid19UpdateEvent(Event):
+    def __init__(self,
+                 confirmed: int,
+                 deaths: int,
+                 recovered: int,
+                 country: Optional[str] = None,
+                 update_time: Optional[datetime] = None,
+                 *args, **kwargs):
+        super().__init__(*args,
+                         confirmed=confirmed,
+                         deaths=deaths,
+                         recovered=recovered,
+                         country=country,
+                         update_time=update_time,
+                         **kwargs)
+
+
+# vim:sw=4:ts=4:et:
---
+++ platypush/plugins/covid19.py
@@ -0,0 +1,64 @@
+from typing import Optional, Union, List, Dict, Any
+
+import requests
+
+from platypush.plugins import Plugin, action
+
+
+class Covid19Plugin(Plugin):
+    """
+    Monitor the diffusion data of the COVID-19 pandemic by using the public API at https://api.covid19api.com.
+    """
+
+    base_url = 'https://api.covid19api.com'
+
+    def __init__(self, country: Union[str, List[str]] = 'world', **kwargs):
+        """
+        :param country: Default country (or list of countries) to retrieve the stats for. It can either be the full
+            country name or the country code. Special values:
+
+            - ``world``: Get worldwide stats (default).
+            - ``all``: Get all the available stats.
+        """
+        super().__init__(**kwargs)
+        self.country = country
+        self.country = self._get_countries(country)
+
+    def _get_countries(self, country: Optional[Union[str, List[str]]] = None) -> List[str]:
+        country = country or self.country
+        if isinstance(country, str):
+            country = country.split(',')
+        return [c.upper().strip() for c in country]
+
+    @action
+    def summary(self, country: Optional[Union[str, List[str]]] = None) -> List[Dict[str, Any]]:
+        """
+        Get the summary data for the world or a country.
+
+        :param country: Default country override.
+        """
+        countries = self._get_countries(country)
+        response = requests.get('{}/summary'.format(self.base_url)).json()
+        if countries[0] == 'all':
+            return response.get('Countries', [])
+        if countries[0] == 'world':
+            return response.get('Global', {})
+
+        return [
+            c for c in response.get('Countries', [])
+            if c.get('CountryCode').upper() in countries
+            or c.get('Country').upper() in countries
+        ]
+
+    @action
+    def data(self, country: Optional[Union[str, List[str]]] = None) -> List[Dict[str, Any]]:
+        """
+        Get all the data for a country.
+
+        :param country: Default country override.
+        """
+        country = self._get_countries(country)[0]
+        return requests.get('{}/total/dayone/country/{}'.format(self.base_url, country)).json()
+
+
+# vim:sw=4:ts=4:et:
