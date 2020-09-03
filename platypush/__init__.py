d27b23ec5a33fbd5b95ec4cd9ad10e9e1cac5be5
Fabio Manganiello <blacklight86@gmail.com>
2020-09-03 17:17:47 +0200

    Bump version: 0.13.4 → 0.13.5

--- platypush/__init__.py
+++ platypush/__init__.py
@@ -24,7 +24,7 @@ from .utils import set_thread_name
 
 
 __author__ = 'Fabio Manganiello <blacklight86@gmail.com>'
-__version__ = '0.13.4'
+__version__ = '0.13.5'
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ backend = pkg_files('platypush/backend')
 
 setup(
     name="platypush",
-    version="0.13.4",
+    version="0.13.5",
     author="Fabio Manganiello",
     author_email="info@fabiomanganiello.com",
     description="Platypush service",
