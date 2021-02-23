d1b7b1768c2f61bdf338377d6c32369833f60bb7
Fabio Manganiello <info@fabiomanganiello.com>
2021-02-23 01:09:03 +0100

    Improved tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 PYTHON=python
+tests_ret=0
 
 for testcase in tests/test_*.py
 do
     $PYTHON -m unittest $testcase
+    test_ret=$?
+
+    if [[ $test_ret != 0 ]]; then
+        tests_ret=$test_ret
+        echo "-------------" >&2
+        echo "Test FAILED: $testcase" >&2
+        echo "-------------" >&2
+    fi
 done
 
-# vim:sw=4:ts=4:et:
+exit $tests_ret
 
+
+# vim:sw=4:ts=4:et:
