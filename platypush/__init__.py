5c9b8efef2850050067f4a41c12b42a10663b59e
Fabio Manganiello <info@fabiomanganiello.com>
2021-06-22 23:40:45 +0200

    Bump version: 0.21.0 → 0.21.1

--- platypush/__init__.py
+++ platypush/__init__.py
@@ -24,7 +24,7 @@ from .utils import set_thread_name
 
 
 __author__ = 'Fabio Manganiello <info@fabiomanganiello.com>'
-__version__ = '0.21.0'
+__version__ = '0.21.1'
 
 logger = logging.getLogger('platypush')
 
