8ec9c8f2034efc10d51c5e6c336d1952f26423a2
Fabio Manganiello <info@fabiomanganiello.com>
2022-04-10 13:07:01 +0200

    Added standard component for icons

---
+++ platypush/backend/http/webapp/src/components/elements/Icon.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="icon-container">
+    <img class="icon" :src="url" :alt="alt" v-if="url?.length">
+    <i class="icon" :class="className" v-else-if="className?.length" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    class: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    alt: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    className() {
+      return this.class
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.icon-container {
+  display: inline-flex;
+  width: $icon-container-size;
+  justify-content: center;
+  text-align: center;
+
+  .icon {
+    width: 1em;
+    height: 1em;
+  }
+}
+</style>
