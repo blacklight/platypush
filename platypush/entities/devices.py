90f067de61e6ea6e7861a72e93b844be63225fa0
Fabio Manganiello <info@fabiomanganiello.com>
2022-04-29 23:27:35 +0200

    Added `reachable` flag to device entities

--- platypush/entities/devices.py
+++ platypush/entities/devices.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, Boolean, ForeignKey
 
 from ._base import Entity
 
@@ -7,6 +7,7 @@ class Device(Entity):
     __tablename__ = 'device'
 
     id = Column(Integer, ForeignKey(Entity.id, ondelete='CASCADE'), primary_key=True)
+    reachable = Column(Boolean, default=True)
 
     __mapper_args__ = {
         'polymorphic_identity': __tablename__,
