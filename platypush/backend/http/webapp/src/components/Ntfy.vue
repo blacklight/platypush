7e31ac6ed86d62417f86c744f15947a07d1065e8
Fabio Manganiello <info@fabiomanganiello.com>
2022-06-02 23:41:50 +0200

    Added missing web build files

---
+++ platypush/backend/http/webapp/src/components/Ntfy.vue
@@ -0,0 +1,28 @@
+<template>
+  <div />
+</template>
+
+<script>
+import Utils from "@/Utils";
+
+export default {
+  name: "Ntfy",
+  mixins: [Utils],
+
+  methods: {
+    onMessage(event) {
+      this.notify({
+        title: event.title,
+        text: event.message,
+        image: {
+          icon: 'bell',
+        },
+      })
+    },
+  },
+
+  mounted() {
+    this.subscribe(this.onMessage, null, 'platypush.message.event.ntfy.NotificationEvent')
+  },
+}
+</script>
