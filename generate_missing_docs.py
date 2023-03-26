efe400f921fea66b8151ca0d2f2444c0a2e73937
Fabio Manganiello <fabio@manganiello.tech>
2023-03-26 22:55:22 +0200

    Fixed `maxdepth` attribute in generate docs.

--- generate_missing_docs.py
+++ generate_missing_docs.py
@@ -16,7 +16,9 @@ def get_all_plugins():
     manifests = {mf.component_name for mf in get_manifests(Plugin)}
     return {
         plugin_name: plugin_info
-        for plugin_name, plugin_info in _get_inspect_plugin().get_all_plugins().output.items()
+        for plugin_name, plugin_info in _get_inspect_plugin()
+        .get_all_plugins()
+        .output.items()
         if plugin_name in manifests
     }
 
@@ -25,7 +27,9 @@ def get_all_backends():
     manifests = {mf.component_name for mf in get_manifests(Backend)}
     return {
         backend_name: backend_info
-        for backend_name, backend_info in _get_inspect_plugin().get_all_backends().output.items()
+        for backend_name, backend_info in _get_inspect_plugin()
+        .get_all_backends()
+        .output.items()
         if backend_name in manifests
     }
 
@@ -40,8 +44,16 @@ def get_all_responses():
 
 # noinspection DuplicatedCode
 def generate_plugins_doc():
-    plugins_index = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'plugins.rst')
-    plugins_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'platypush', 'plugins')
+    plugins_index = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'plugins.rst'
+    )
+    plugins_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'docs',
+        'source',
+        'platypush',
+        'plugins',
+    )
     all_plugins = sorted(plugin for plugin in get_all_plugins().keys())
 
     for plugin in all_plugins:
@@ -57,15 +69,17 @@ def generate_plugins_doc():
                 f.write(out)
 
     with open(plugins_index, 'w') as f:
-        f.write('''
+        f.write(
+            '''
 Plugins
 =======
 
 .. toctree::
-    :maxdepth: 2
+    :maxdepth: 1
     :caption: Plugins:
 
-''')
+'''
+        )
 
         for plugin in all_plugins:
             f.write('    platypush/plugins/' + plugin + '.rst\n')
@@ -73,8 +87,16 @@ Plugins
 
 # noinspection DuplicatedCode
 def generate_backends_doc():
-    backends_index = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'backends.rst')
-    backends_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'platypush', 'backend')
+    backends_index = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'backends.rst'
+    )
+    backends_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'docs',
+        'source',
+        'platypush',
+        'backend',
+    )
     all_backends = sorted(backend for backend in get_all_backends().keys())
 
     for backend in all_backends:
@@ -90,15 +112,17 @@ def generate_backends_doc():
                 f.write(out)
 
     with open(backends_index, 'w') as f:
-        f.write('''
+        f.write(
+            '''
 Backends
 ========
 
 .. toctree::
-    :maxdepth: 2
+    :maxdepth: 1
     :caption: Backends:
 
-''')
+'''
+        )
 
         for backend in all_backends:
             f.write('    platypush/backend/' + backend + '.rst\n')
@@ -107,8 +131,17 @@ Backends
 # noinspection DuplicatedCode
 def generate_events_doc():
     from platypush.message import event as event_module
-    events_index = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'events.rst')
-    events_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'platypush', 'events')
+
+    events_index = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'events.rst'
+    )
+    events_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'docs',
+        'source',
+        'platypush',
+        'events',
+    )
     all_events = sorted(event for event in get_all_events().keys() if event)
 
     for event in all_events:
@@ -116,22 +149,26 @@ def generate_events_doc():
         if not os.path.exists(event_file):
             header = '``{}``'.format(event)
             divider = '=' * len(header)
-            body = '\n.. automodule:: {}.{}\n    :members:\n'.format(event_module.__name__, event)
+            body = '\n.. automodule:: {}.{}\n    :members:\n'.format(
+                event_module.__name__, event
+            )
             out = '\n'.join([header, divider, body])
 
             with open(event_file, 'w') as f:
                 f.write(out)
 
     with open(events_index, 'w') as f:
-        f.write('''
+        f.write(
+            '''
 Events
 ======
 
 .. toctree::
-    :maxdepth: 2
+    :maxdepth: 1
     :caption: Events:
 
-''')
+'''
+        )
 
         for event in all_events:
             f.write('    platypush/events/' + event + '.rst\n')
@@ -140,31 +177,46 @@ Events
 # noinspection DuplicatedCode
 def generate_responses_doc():
     from platypush.message import response as response_module
-    responses_index = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'responses.rst')
-    responses_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'platypush', 'responses')
-    all_responses = sorted(response for response in get_all_responses().keys() if response)
+
+    responses_index = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'docs', 'source', 'responses.rst'
+    )
+    responses_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'docs',
+        'source',
+        'platypush',
+        'responses',
+    )
+    all_responses = sorted(
+        response for response in get_all_responses().keys() if response
+    )
 
     for response in all_responses:
         response_file = os.path.join(responses_dir, response + '.rst')
         if not os.path.exists(response_file):
             header = '``{}``'.format(response)
             divider = '=' * len(header)
-            body = '\n.. automodule:: {}.{}\n    :members:\n'.format(response_module.__name__, response)
+            body = '\n.. automodule:: {}.{}\n    :members:\n'.format(
+                response_module.__name__, response
+            )
             out = '\n'.join([header, divider, body])
 
             with open(response_file, 'w') as f:
                 f.write(out)
 
     with open(responses_index, 'w') as f:
-        f.write('''
+        f.write(
+            '''
 Responses
 =========
 
 .. toctree::
-    :maxdepth: 2
+    :maxdepth: 1
     :caption: Responses:
 
-''')
+'''
+        )
 
         for response in all_responses:
             f.write('    platypush/responses/' + response + '.rst\n')
