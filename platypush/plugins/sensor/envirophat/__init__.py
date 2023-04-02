7697c1c6ad601076a9b99a84b3f58fdc57d7ee48
Fabio Manganiello <fabio@manganiello.tech>
2023-04-02 02:49:08 +0200

    Migrated `envirophat` to the new `SensorPlugin` API.
    Removed `backend.sensor.envirophat` and `gpio.sensor.envirophat` plugin.
    They have now been merged into the new `sensor.envirophat` plugin.

--- platypush/plugins/sensor/envirophat/__init__.py
+++ platypush/plugins/sensor/envirophat/__init__.py
@@ -1,23 +1,110 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Type
+from typing_extensions import override
+
+from platypush.common.sensors import Numeric
+from platypush.entities.acceleration import Accelerometer
+from platypush.entities.devices import Device
+from platypush.entities.distance import DistanceSensor
+from platypush.entities.illuminance import IlluminanceSensor
+from platypush.entities.magnetism import Magnetometer
+from platypush.entities.pressure import PressureSensor
+from platypush.entities.sensors import BinarySensor, RawSensor, Sensor
+from platypush.entities.temperature import TemperatureSensor
 from platypush.plugins import action
-from platypush.plugins.gpio.sensor import GpioSensorPlugin
+from platypush.plugins.sensor import SensorPlugin
 
 
-class GpioSensorEnvirophatPlugin(GpioSensorPlugin):
+@dataclass
+class SensorEntityMapping:
     """
-    Plugin to interact with a `Pimoroni enviropHAT <https://shop.pimoroni.com/products/enviro-phat>`_ device.
-    You can use an enviropHAT device to read e.g. temperature, pressure, altitude, accelerometer, magnetometer and
-    luminosity data, plus control the status of its RGB LEDs.
+    Maps sensor attributes returned by
+    :meth:`SensorEnvirophatPlugin.get_measurement` to native sensor entities.
+    """
+
+    name: str
+    entity_type: Type[Sensor]
+    unit: Optional[str] = None
+    kwargs: Dict[str, Any] = field(default_factory=dict)
+
+    @property
+    def id(self):
+        """
+        Standard format for an Envirophat sensor external ID.
+        """
+        return f'envirophat:{self.name}'
+
+
+_sensor_entity_mappings: Dict[str, SensorEntityMapping] = {
+    mapping.name: mapping
+    for mapping in [
+        SensorEntityMapping(
+            name='temperature',
+            entity_type=TemperatureSensor,
+            unit='°C',
+        ),
+        SensorEntityMapping(
+            name='pressure',
+            entity_type=PressureSensor,
+            unit='Pa',
+        ),
+        SensorEntityMapping(
+            name='altitude',
+            entity_type=DistanceSensor,
+            unit='m',
+        ),
+        SensorEntityMapping(
+            name='illuminance',
+            entity_type=IlluminanceSensor,
+            unit='lux',
+        ),
+        SensorEntityMapping(
+            name='accelerometer',
+            entity_type=Accelerometer,
+        ),
+        SensorEntityMapping(
+            name='magnetometer',
+            entity_type=Magnetometer,
+        ),
+        SensorEntityMapping(
+            name='analog',
+            entity_type=RawSensor,
+        ),
+        SensorEntityMapping(
+            name='leds',
+            entity_type=BinarySensor,
+        ),
+    ]
+}
+
+
+# pylint: disable=too-many-ancestors
+class SensorEnvirophatPlugin(SensorPlugin):
+    """
+    Plugin to interact with a `Pimoroni enviropHAT
+    <https://shop.pimoroni.com/products/enviro-phat>`_ device.
+
+    You can use an enviropHAT device to read e.g. temperature, pressure,
+    altitude, accelerometer, magnetometer and luminosity data.
 
     Requires:
 
         * ``envirophat`` (``pip install envirophat``)
+
+    Triggers:
+
+        * :class:`platypush.message.event.sensor.SensorDataAboveThresholdEvent`
+        * :class:`platypush.message.event.sensor.SensorDataBelowThresholdEvent`
+        * :class:`platypush.message.event.sensor.SensorDataChangeEvent`
+
     """
 
+    @override
     @action
-    def get_measurement(self, qnh=1020.0):
+    def get_measurement(self, *_, qnh: float = 1020.0, **__):
         """
-        :param: qnh: Local value for atmospheric pressure adjusted to sea level (default: 1020)
-        :type qnh: float
+        :param: qnh: Local value for atmospheric pressure adjusted to sea level
+            (default: 1020)
 
         :returns: dict. Example:
 
@@ -68,6 +155,25 @@ class GpioSensorEnvirophatPlugin(GpioSensorPlugin):
         ret['leds'] = leds.is_on()
 
         return ret
+
+    @override
+    def transform_entities(self, entities: Dict[str, Numeric]) -> List[Device]:
+        return [
+            Device(
+                id='envirophat',
+                name='EnviroPHAT',
+                children=[
+                    _sensor_entity_mappings[sensor].entity_type(
+                        id=_sensor_entity_mappings[sensor].id,
+                        name=_sensor_entity_mappings[sensor].name,
+                        unit=_sensor_entity_mappings[sensor].unit,
+                        **_sensor_entity_mappings[sensor].kwargs,
+                    )
+                    for sensor, value in entities.items()
+                    if value is not None
+                ],
+            )
+        ]
 
 
 # vim:sw=4:ts=4:et:
