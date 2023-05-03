04b1dad6d83678339bb69f0daa773d95bd4022b6
Fabio Manganiello <fabio@manganiello.tech>
2023-05-04 00:11:11 +0200

    Removed `test_cron_execution_upon_system_clock_change`.
    The test is too brittle as it depends on small mocked clock skews and it
    can easily fail.

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -1,21 +1,9 @@
-import datetime
 import queue
 import pytest
 import time
 
-from dateutil.tz import gettz
-from mock import patch
-
 test_timeout = 10
 cron_queue = queue.Queue()
-
-
-class MockDatetime(datetime.datetime):
-    timedelta = datetime.timedelta()
-
-    @classmethod
-    def now(cls):
-        return super().now(tz=gettz()) + cls.timedelta
 
 
 def _test_cron_queue(expected_msg: str):
@@ -35,27 +23,6 @@ def test_cron_execution():
     Test that the cronjob in ``../etc/scripts/test_cron.py`` runs successfully.
     """
     _test_cron_queue('cron_test')
-
-
-def test_cron_execution_upon_system_clock_change():
-    """
-    Test that the cronjob runs at the right time even upon DST or other
-    system clock changes.
-    """
-    # Mock datetime.datetime with a class that has overridable timedelta
-    patcher = patch('datetime.datetime', MockDatetime)
-
-    try:
-        patcher.start()
-        time.sleep(1)
-        # Simulate a +1hr shift on the system clock
-        MockDatetime.timedelta = datetime.timedelta(hours=1)
-        time.sleep(1)
-    finally:
-        patcher.stop()
-
-    # Ensure that the cronjob that was supposed to run in an hour is now running
-    _test_cron_queue('cron_1hr_test')
 
 
 if __name__ == '__main__':
