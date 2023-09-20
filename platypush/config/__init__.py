453b6becaef90d4f77ae056a39c91ed7d9c9f864
Fabio Manganiello <fabio@manganiello.tech>
2023-09-21 01:18:15 +0200

    FIX: We shouldn't expand `cfgfile` if it's empty.

--- platypush/config/__init__.py
+++ platypush/config/__init__.py
@@ -106,7 +106,9 @@ class Config:
         if cfgfile is None:
             cfgfile = self._get_default_cfgfile()
 
-        cfgfile = os.path.abspath(os.path.expanduser(cfgfile))
+        if cfgfile:
+            cfgfile = os.path.abspath(os.path.expanduser(cfgfile))
+
         if cfgfile is None or not os.path.exists(cfgfile):
             cfgfile = self._create_default_config(cfgfile)
 
