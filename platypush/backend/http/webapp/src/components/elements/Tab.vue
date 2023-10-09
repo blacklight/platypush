84efef710ee9dcc0225f0831182d03d4bc0cafd5
Fabio Manganiello <fabio@manganiello.tech>
2023-10-10 00:39:27 +0200

    Added `Tabs` and `Tab` UI elements.

---
+++ platypush/backend/http/webapp/src/components/elements/Tab.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tab" :class="selected ? 'selected' : ''" @click="$emit('input')">
+    <span class="icon" v-if="iconClass?.length || iconUrl?.length">
+      <Icon :class="iconClass" :url="iconUrl" />
+    </span> &nbsp;
+    <slot />
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/elements/Icon";
+
+export default {
+  name: "Tab",
+  components: {Icon},
+  emits: ['input'],
+  props: {
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+
+    iconClass: {
+      type: String,
+    },
+
+    iconUrl: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  background: $tab-bg;
+  display: flex;
+  padding: 1em;
+  align-items: center;
+  border-right: $default-border-2;
+  cursor: pointer;
+
+  &.selected {
+    background: $selected-bg;
+    font-weight: bold;
+  }
+
+  &:hover {
+    background: $hover-bg;
+  }
+
+  :deep(.icon-container) {
+    width: 1.5em;
+  }
+}
+</style>
