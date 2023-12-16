3bc27a505f88124021f69cb365d79cf11bb9fda9
Fabio Manganiello <fabio@manganiello.tech>
2023-12-16 18:58:31 +0100

    [UI] setUrlArgs should remove args when value == null.

--- platypush/backend/http/webapp/src/utils/Url.vue
+++ platypush/backend/http/webapp/src/utils/Url.vue
@@ -22,10 +22,22 @@ export default {
     },
 
     setUrlArgs(args) {
-      args = {...this.getUrlArgs(), ...args}
-      window.location.href = (
-        `${window.location.pathname}#${this.parseUrlFragment()}?${this.fragmentFromArgs(args)}`
-      )
+      const curArgs = this.getUrlArgs()
+      args = Object.entries(args)
+        .reduce((acc, [key, value]) => {
+          if (value != null)
+            acc[key] = value
+          else if (curArgs[key] != null)
+            delete curArgs[key]
+          return acc
+        }, {})
+
+      args = {...curArgs, ...args}
+      let location = `${window.location.pathname}#${this.parseUrlFragment()}`
+      if (Object.keys(args).length)
+        location += `?${this.fragmentFromArgs(args)}`
+
+      window.location.href = location
     },
 
     fragmentFromArgs(args) {
