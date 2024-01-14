bf1b8aaf70627dbc57e90e6cf6c3e5ab3f80a196
Fabio Manganiello <fabio@manganiello.tech>
2024-01-14 21:27:08 +0100

    Delete pubsub closure to `_pubsub_close()`.
    The Redis channel may be closed, and in that case we need to handle the
    exception instead of throwing it - which causes the upstream Tornado
    worker to fail too.

--- platypush/backend/http/app/mixins/__init__.py
+++ platypush/backend/http/app/mixins/__init__.py
@@ -65,8 +65,7 @@ class PubSubMixin:
         with self._pubsub_lock:
             # Close and free the pub/sub object if it has no active subscriptions.
             if self._pubsub is not None and len(self._subscriptions) == 0:
-                self._pubsub.close()
-                self._pubsub = None
+                self._pubsub_close()
 
     @staticmethod
     def _serialize(data: MessageType) -> bytes:
