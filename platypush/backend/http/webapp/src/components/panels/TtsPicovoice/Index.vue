61e466d8e2fc74711320ec262538ab99476e16ed
Fabio Manganiello <fabio@manganiello.tech>
2024-05-02 03:07:47 +0200

    [UI] Added `tts.picovoice` component and `assistant.picovoice` icon.

---
+++ platypush/backend/http/webapp/src/components/panels/TtsPicovoice/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Panel plugin-name="tts.picovoice" />
+</template>
+
+<script>
+import Panel from "@/components/panels/Tts/Panel";
+
+export default {
+  name: "Tts",
+  components: {Panel}
+}
+</script>
