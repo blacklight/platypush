e23664b5e7ad2fac9540b97d19ee0d71390d28f1
Fabio Manganiello <fabio@manganiello.tech>
2024-05-26 00:56:01 +0200

    [CI/CD] Be explicit about the origin in sync-stable-branch.

--- .drone/sync-stable-branch.sh
+++ .drone/sync-stable-branch.sh
@@ -4,11 +4,15 @@
 . .drone/macros/configure-ssh.sh
 . .drone/macros/configure-gpg.sh
 
+# Git configuration
+git remote rm origin
+git remote add origin git@git.platypush.tech:platypush/platypush.git
+
 # Merge and push to the `stable` branch
 git checkout stable || git checkout -b stable
-git rebase master
+git rebase origin master
 git push -u origin stable
-git checkout master
+git checkout origin master
 
 # Restore the original git configuration
 mv "$TMP_GIT_CONF" "$GIT_CONF"
