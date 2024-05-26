d6d9d7a8e7bab4b0fc41de6ef71a6788f02b88c7
Fabio Manganiello <fabio@manganiello.tech>
2024-05-26 03:40:10 +0200

    [CI/CD] Added extra dependencies to `update-pip-package` step.

--- .drone/update-pip-package.sh
+++ .drone/update-pip-package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-apk add --update --no-cache py3-twine
+apk add --update --no-cache py3-twine py3-setuptools py3-wheel py3-pip
 python setup.py sdist bdist_wheel
 twine upload dist/platypush-$(python setup.py --version).tar.gz
