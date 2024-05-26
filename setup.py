0ffff854d3a6a8e2810f357adbe4a397110e0f7c
Fabio Manganiello <fabio@manganiello.tech>
2024-05-26 03:18:16 +0200

    Bump version: 0.99.7 → 0.99.8

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ backend = pkg_files('platypush/backend')
 
 setup(
     name="platypush",
-    version="0.99.7",
+    version="0.99.8",
     author="Fabio Manganiello",
     author_email="fabio@manganiello.tech",
     description="Platypush service",
