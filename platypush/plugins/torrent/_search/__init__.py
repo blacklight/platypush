91e2530dd5bfbd9dd1e3fa77a350ecb5c5c4e53a
Fabio Manganiello <fabio@manganiello.tech>
2024-06-23 01:35:46 +0200

    [#407] Implemented torrent.csv backend

--- platypush/plugins/torrent/_search/__init__.py
+++ platypush/plugins/torrent/_search/__init__.py
@@ -1,10 +1,12 @@
 from ._base import TorrentSearchProvider
 from ._popcorntime import PopcornTimeSearchProvider
+from ._torrents_csv import TorrentsCsvSearchProvider
 
 
 __all__ = [
     'TorrentSearchProvider',
     'PopcornTimeSearchProvider',
+    'TorrentsCsvSearchProvider',
 ]
 
 
