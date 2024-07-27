0963cd3d55d3ba20846c2dcfdd3504993c237b7b
Fabio Manganiello <fabio@manganiello.tech>
2024-07-27 12:12:07 +0200

    Moving around the __version__ string to get bumpversion to work again.

--- platypush/__init__.py
+++ platypush/__init__.py
@@ -16,13 +16,12 @@ from .message.response import Response
 from .procedure import procedure
 from .runner import main
 from .utils import run
-from .__version__ import __version__
 
 # Alias for platypush.event.hook.hook,
 # see https://git.platypush.tech/platypush/platypush/issues/399
 when = hook
 
-
+__version__ = '1.1.3'
 __author__ = 'Fabio Manganiello <fabio@manganiello.tech>'
 __all__ = [
     'Application',
