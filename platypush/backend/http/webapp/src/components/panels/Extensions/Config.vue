e5a2127ecb75648fecbd3365aef326fb032bd065
Fabio Manganiello <fabio@manganiello.tech>
2024-09-22 02:39:43 +0200

    [UI] s/stackoverflow-dark/monokai-sublime/ in extensions config page.
    The dark blue YAML style used by `stackoverflow-dark` isn't very
    readable on black background.

--- platypush/backend/http/webapp/src/components/panels/Extensions/Config.vue
+++ platypush/backend/http/webapp/src/components/panels/Extensions/Config.vue
@@ -13,7 +13,7 @@
 
 <script>
 import 'highlight.js/lib/common'
-import 'highlight.js/styles/stackoverflow-dark.min.css'
+import 'highlight.js/styles/monokai-sublime.min.css'
 import hljs from "highlight.js"
 import CopyButton from "@/components/elements/CopyButton"
 import Utils from "@/Utils";
