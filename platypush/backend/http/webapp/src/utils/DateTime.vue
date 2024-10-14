4f81a73fb93d9e41b2a2e8bc9bead31d94d48a72
Fabio Manganiello <fabio@manganiello.tech>
2024-10-14 21:46:49 +0200

    [UI] Added formatDuration utility.

--- platypush/backend/http/webapp/src/utils/DateTime.vue
+++ platypush/backend/http/webapp/src/utils/DateTime.vue
@@ -35,6 +35,22 @@ export default {
 
       return `${this.formatDate(date, year)}, ${this.formatTime(date, seconds)}`
     },
+
+    formatDuration(duration, seconds=true) {
+      if (duration == null)
+        return ''
+
+      let hours = Math.floor(duration / 3600)
+      let minutes = Math.floor((duration % 3600) / 60)
+      let secs = duration % 60
+
+      if (hours > 0)
+        return `${hours}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
+      else if (minutes > 0)
+        return `${minutes}:${secs.toString().padStart(2, '0')}`
+      else
+        return seconds ? `0:${secs.toString().padStart(2, '0')}` : `0:00`
+    },
   },
 }
 </script>
