4015cf356dac6ab585bb9569768bc38c62a54663
Fabio Manganiello <fabio@manganiello.tech>
2024-10-19 15:05:19 +0000

    [Automatic] Updated UI files

--- platypush/backend/http/webapp/dist/static/js/2305.d6f27d19.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkplatypush"]=self["webpackChunkplatypush"]||[]).push([[2305],{2305:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var u=t(6252);function a(e,n,t,a,r,c){const s=(0,u.up)("Media");return(0,u.wg)(),(0,u.j4)(s,{"plugin-name":"media.chromecast"})}var r=t(4669),c={components:{Media:r["default"]}},s=t(3744);const p=(0,s.Z)(c,[["render",a]]);var i=p}}]);
-//# sourceMappingURL=2305.d6f27d19.js.map
