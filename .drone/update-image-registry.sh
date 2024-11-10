8bc5e5b8c581828efb3e4bc0a0c3630a7db910c4
Fabio Manganiello <fabio@manganiello.tech>
2024-11-10 15:23:04 +0100

    [#434] Added pipeline to update registry images.

---
+++ .drone/update-image-registry.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export VERSION=$(grep current_version pyproject.toml | sed -r -e "s/.*=\s*['\"]?([^'\"]+)['\"]?\s*$/\1/")
+export IMAGE_NAME="$REGISTRY_ENDPOINT/$DOCKER_USER/platypush"
+
+git config --global --add safe.directory /app
+docker login "$REGISTRY_ENDPOINT" -u "$DOCKER_USER" -p "$DOCKER_PASS"
+docker build -f Dockerfile.alpine -t "$IMAGE_NAME:$VERSION" .
+docker tag "$IMAGE_NAME:$VERSION" "$IMAGE_NAME:latest"
+
+docker push "$IMAGE_NAME:$VERSION"
+docker push "$IMAGE_NAME:latest"
