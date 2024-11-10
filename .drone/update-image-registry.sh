2dee5a25ecbaed647ebc41fd1852fd80f09694af
Fabio Manganiello <fabio@manganiello.tech>
2024-11-10 21:54:52 +0100

    Removed unrequired git command from `update-image-registry.sh`.

--- .drone/update-image-registry.sh
+++ .drone/update-image-registry.sh
@@ -3,7 +3,6 @@
 export VERSION=$(grep current_version pyproject.toml | sed -r -e "s/.*=\s*['\"]?([^'\"]+)['\"]?\s*$/\1/")
 export IMAGE_NAME="$REGISTRY_ENDPOINT/$DOCKER_USER/platypush"
 
-git config --global --add safe.directory /app
 docker login "$REGISTRY_ENDPOINT" -u "$DOCKER_USER" -p "$DOCKER_PASS"
 docker build -f Dockerfile.alpine -t "$IMAGE_NAME:$VERSION" .
 docker tag "$IMAGE_NAME:$VERSION" "$IMAGE_NAME:latest"
