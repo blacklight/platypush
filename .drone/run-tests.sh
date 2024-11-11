197e1e91dd1d28e111088b7656d980424eb94497
Fabio Manganiello <fabio@manganiello.tech>
2024-11-11 23:45:44 +0100

    [Build] Removed testing repo from Alpine images.
    The testing repo is only required by `py3-marshmallow`, which is not yet
    included in the community repo, but it can end up breaking some builds
    because of the incompatibility with the packages in the base repo.

--- .drone/run-tests.sh
+++ .drone/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk add --update --no-cache --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/ $(cat platypush/install/requirements/alpine.txt)
+apk add --update --no-cache $(cat platypush/install/requirements/alpine.txt)
 pip install . --break-system-packages
 pip install -r requirements-tests.txt --break-system-packages
 pytest tests
