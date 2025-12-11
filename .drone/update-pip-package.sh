ecc68ed37b5c4b94e09c42e26f4d8d8e4e5e6aeb
Fabio Manganiello <fabio@manganiello.tech>
2025-12-11 02:56:38 +0100

    Added py3-build to update-pip-package image

--- .drone/update-pip-package.sh
+++ .drone/update-pip-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk add --update --no-cache py3-twine py3-setuptools py3-wheel py3-pip
+apk add --update --no-cache py3-twine py3-setuptools py3-wheel py3-pip py3-build
 rm -rf build
 python -m build
 twine upload dist/platypush-$(python setup.py --version).tar.gz
