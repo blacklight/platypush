cf21f714d47d0107c179aece898bc04ceed2ac92
Fabio Manganiello <fabio@manganiello.tech>
2025-12-11 03:02:04 +0100

    Bump version: 1.3.14 → 1.3.15

--- platypush/__init__.py
+++ platypush/__init__.py
@@ -21,7 +21,7 @@ from .utils import run
 # see https://git.platypush.tech/platypush/platypush/issues/399
 when = hook
 
-__version__ = '1.3.14'
+__version__ = '1.3.15'
 __author__ = 'Fabio Manganiello <fabio@manganiello.tech>'
 __all__ = [
     'Application',
