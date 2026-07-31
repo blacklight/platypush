4a1aad1a5bcf9f0e067c09bbdf11c6c6bbf08e0f
Fabio Manganiello <fabio@manganiello.tech>
2026-07-31 11:30:39 +0200

    Bump version: 1.3.32 → 1.3.33

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [1.3.33]
 
 ### Added
 
--- platypush/__init__.py
+++ platypush/__init__.py
@@ -21,7 +21,7 @@ from .utils import run
 # see https://git.platypush.tech/platypush/platypush/issues/399
 when = hook
 
-__version__ = '1.3.32'
+__version__ = '1.3.33'
 __author__ = 'Fabio Manganiello <fabio@manganiello.tech>'
 __all__ = [
     'Application',
