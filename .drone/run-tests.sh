2885b19263fa86df17e5d44695086d8d17eb60a2
Fabio Manganiello <fabio@manganiello.tech>
2026-08-02 02:34:45 +0200

    ci(tests): Added py3-cryptography and openssl to system deps

--- .drone/run-tests.sh
+++ .drone/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk add --update --no-cache $(cat platypush/install/requirements/alpine.txt)
+apk add --update --no-cache $(cat platypush/install/requirements/alpine.txt) py3-cryptography openssl
 pip install . --break-system-packages
 pip install -r requirements-tests.txt --break-system-packages
 pytest tests
